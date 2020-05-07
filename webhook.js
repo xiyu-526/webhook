bb0d4bf25c981b6cc7515d7cd04b20cd1bcc5444
xiyu-526 <997578871@qq.com>
2020-05-07 17:04:47 +0800

    1

--- webhook.js
+++ webhook.js
@@ -1,10 +1,11 @@
 let app = require("http");
 
 let server = app.createServer(function(req,res){
+    console.log(req.method,req.url);
     if(req.method === "POST" && req.url === "/webhook"){
         //设置回应格式 （Json）
         res.setHeader('Content-Type','application/json');
-        res.end({status:'ok'});
+        res.end(JSON.stringify({status:'ok'}));
     }else{
         res.end("Not Found 404");
     }
