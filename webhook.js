f9341e3194d2594e4ca507ec53e0a00a10254b2c
xiyu-526 <63832443+xiyu-526@users.noreply.github.com>
2020-05-10 10:00:06 +0800

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,6 +1,7 @@
 let app = require("http");
 let {spawn} = require("child_process");
 let crypto = require('crypto'); //hash 模块
+//let sendMail = require('./sendMail');
 function sign(body){
     //使用 hash.update() 方法将要计算的数据以流（stream）的方式写入，流输入结束后，使用 hash.digest() 方法计算数据的 hash 值。
     //sha1双重加密
@@ -10,10 +11,11 @@ function sign(body){
 }
 
 let server = app.createServer(function(req,res){
-    console.log(req.method,req.url);
+    console.log(req.method,req.url,req.headers['x-github-event'],req.headers['x-hub-signature']);
     if(req.method === "POST" && req.url === "/webhook"){
         let bufs = [];
         req.on('data',function(buf){
+            console.log("req-data:"+buf);
             bufs.push(buf);
         })
         req.on('end',function(buf){
@@ -21,7 +23,10 @@ let server = app.createServer(function(req,res){
             //触发传递的事件类型的名称。
             let eventname = req.headers['x-github-event']; //push事件
             let signature = req.headers['x-hub-signature']; //github请求过来的时候，要传递请求体body,还会传递一个signature(签名)，你需要验证签名对不对
-
+            
+            console.log("signature:"+signature);
+            console.log("BODY:"+body);
+            console.log("sign-BODY:"+sign(body));
             //sign 用于生成签名
             if(signature !== sign(body)){
                return res.end("没找到");
@@ -34,11 +39,20 @@ let server = app.createServer(function(req,res){
                 let child = spawn('sh',[`${pay.repository.name}.sh`]);
                 let bufs = [];
                 child.stdout.on("data",function(buffer){
+                    console.log("childe-data-on:"+buffer);
                     bufs.push(buffer);
                 })
                 child.stdout.on("end",function(buffer){
                     let logs = Buffer.concat(bufs);
                     console.log(logs);
+                    let tologs = logs.toString()
+                    sendMail(`
+                        <h3>部署日期：${new Date()}</h3>
+                        <h3>部署人：${pay.pusher.name}</h3>
+                        <h3>部署邮箱：${pay.pusher.email}</h3>
+                        <h3>提交信息：${pay.head_commit&&pay.head_commit['message']}</h3>
+                        <h3>部署日志：${tologs.replace("\r\n","<br />")}</h3>
+                    `);
                 })
             } 
             
