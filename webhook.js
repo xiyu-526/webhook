361788221df8684abee7a9eaffe670ba0bb7e974
xiyu-526 <63832443+xiyu-526@users.noreply.github.com>
2020-05-10 08:48:20 +0800

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,7 +1,7 @@
 let app = require("http");
 
 
-let server = app.creatServer(function(req,res){
+let server = app.createServer(function(req,res){
     console.log(req.method,req.url)
     if(req.method === "POST" && req.url==="/webhook"){
         console.log("监听到触发事件")
