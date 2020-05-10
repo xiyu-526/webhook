d41615cc2fd2a4f459c50dde3853be8ebe5eccd2
xiyu-526 <63832443+xiyu-526@users.noreply.github.com>
2020-05-10 17:22:13 +0800

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -10,17 +10,6 @@ function sign(body){
     return `sha1=`+crypto.createHmac('sha1','111111').update(body).digest('hex');
 }
 
-      let json = {
-        name:'adasd',
-        a:[
-          'a1321',123123
-        ]
-      };
-let buffer=Buffer(json);
-      let data = JSON.parse(buffer);
-
-      console.log(data);
-
 let server = app.createServer(function(req,res){
     console.log(req.method,req.url);
     if(req.method === "POST" && req.url === "/webhook"){
