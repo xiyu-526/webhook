e2ef9fb53ad0f24ca51c491bd20f33aa351c767c
xiyu-526 <63832443+xiyu-526@users.noreply.github.com>
2020-05-10 09:44:50 +0800

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,4 +1,5 @@
 let app = require("http");
+let {spawn} = require("child_process");
 let crypto = require('crypto'); //hash 模块
 function sign(body){
     //使用 hash.update() 方法将要计算的数据以流（stream）的方式写入，流输入结束后，使用 hash.digest() 方法计算数据的 hash 值。
@@ -18,17 +19,31 @@ let server = app.createServer(function(req,res){
         req.on('end',function(buf){
             let body = Buffer.concat(bufs);
             //触发传递的事件类型的名称。
-            let eventname = req.header['x-gitHub-event']; //push事件
-            let signature = req.header['x-hub-signature']; //github请求过来的时候，要传递请求体body,还会传递一个signature(签名)，你需要验证签名对不对
+            let eventname = req.headers['x-github-event']; //push事件
+            let signature = req.headers['x-hub-signature']; //github请求过来的时候，要传递请求体body,还会传递一个signature(签名)，你需要验证签名对不对
 
             //sign 用于生成签名
             if(signature !== sign(body)){
                return res.end("没找到");
             }
+            //设置回应格式 （Json）
+            res.setHeader('Content-Type','application/json');
+            res.end(JSON.stringify({status:'ok'}));
+            if(eventname==="push"){
+                let pay = JSON.parse(body);
+                let child = spawn('sh',[`${pay.repository.name}.sh`]);
+                let bufs = [];
+                child.stdout.on("data",function(buffer){
+                    bufs.push(buffer);
+                })
+                child.stdout.on("end",function(buffer){
+                    let logs = Buffer.concat(bufs);
+                    console.log(logs);
+                })
+            } 
+            
         })
-        //设置回应格式 （Json）
-        res.setHeader('Content-Type','application/json');
-        res.end(JSON.stringify({status:'ok'}));
+        
     }else{
         res.end("Not Found 404");
     }
