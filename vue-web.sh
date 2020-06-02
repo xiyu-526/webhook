69bfd8effe8dd669c4184b443d3dea47a779fbd6
xiyu-526 <997578871@qq.com>
2020-06-02 15:25:04 +0800

    修改配置

--- vue-web.sh
+++ vue-web.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd '/usr/mypj/web-vue'
+cd '/usr/mypj/vue-web'
 
 git reset --hard origin/master
 git clean -f
@@ -8,9 +8,9 @@ git pull origin master
 
 npm run build
 
-docker build -t web-vue:v1.0
+docker build -t vue-web:v1.0
 
-docker stop web-vue-container
-docker rm web-vue-container
+docker stop vue-web-container
+docker rm vue-web-container
 
-docker container run -p 80:80 --name web-vue-container -d web-vue:v1.0
+docker container run -p 80:80 --name vue-web-container -d vue-web:v1.0
