ca05dd2973b0a2bdc36712d86a0bada1cf82691a
xiyu-526 <997578871@qq.com>
2020-06-16 16:41:36 +0800

    修改配置

--- react-web2.sh
+++ react-web2.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-WORK_PATH = '/usr/mypj/react-web2'
+WORK_PATH='/usr/mypj/react-web2'
 cd $WORK_PATH
 
+ls
+
+echo "先清除老代码"
 git reset --hard origin/master
 git clean -f
 
+echo "拉取最新代码"
 git pull origin master
 
+echo "install代码"
 npm install
+
+echo "构建代码"
 npm run build
 
+echo "构建镜像"
 docker build -t react-web2:v1.0 .
 
+echo "停止容器"
 docker stop react-web2-container
+
+echo "删除容器"
 docker rm react-web2-container
 
+echo "启动容器"
 docker container run -p 86:86 --name react-web2-container -d react-web2:v1.0
 
